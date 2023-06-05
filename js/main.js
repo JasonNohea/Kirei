1f857afee1269fc8c93b645ea095dfc90c4be445
JasonNohea <101155904+JasonNohea@users.noreply.github.com>
2023-06-05 11:00:07 +0700

    Revert "Adding Login Fadein and Out"
    This reverts commit b06df85b6b839ce0f76d01f29b1d7a9decaeb94f.

--- js/main.js
+++
@@ -1,8 +0,0 @@
-
-$(".login-button").on("click", function () {
-    $(".login-con").fadeToggle("fast", "linear");
-});
-
-$(".dark-screen").on("click", function () {
-    $(".login-con").fadeToggle("fast", "linear");
-});
