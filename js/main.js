0b0a080138f9fc54f85f442d067d281fbdec47c2
JasonNohea <101155904+JasonNohea@users.noreply.github.com>
2023-06-05 11:08:32 +0700

    recommit just in case something goes horrible

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+$(".login-button").on("click", function () {
+    $(".login-con").fadeToggle("fast", "linear");
+});
+
+$(".dark-screen").on("click", function () {
+    $(".login-con").fadeToggle("fast", "linear");
+});
