6efbbe64b9500bdfb4461ff9d11c4c140a8c348c
Jason Nohea <jason.nohea2002@gmail.com>
2023-06-08 09:38:47 +0700

    repair blur

--- js/main.js
+++ js/main.js
@@ -7,3 +7,27 @@ $(".dark-screen").on("click", function () {
     $(".login-con").fadeToggle("fast", "linear");
 });
 
+$(".login-cancel").on("click", function () {
+    $(".login-con").fadeToggle("fast", "linear");
+});
+
+$('.login-submit').on('click', function() {
+    var username = $('#email').val();
+    var password = $('#pass').val();
+
+    if (username === '') {
+      $('#usernameError').removeClass('hidden').text('Please fill in the username field.');
+    } else {
+      $('#usernameError').addClass('hidden');
+    }
+
+    if (password === '') {
+      $('#passwordError').removeClass('hidden').text('Please fill in the password field.');
+    } else {
+      $('#passwordError').addClass('hidden');
+    }
+
+    if (username !== '' && password !== '') {
+      // Perform login logic here
+    }
+  });
