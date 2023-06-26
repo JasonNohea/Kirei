1e2ff64dfb0fe799bd649a45d926721561584c5f
Pistonfry <christianfiestyawan@gmail.com>
2023-06-26 10:58:52 +0700

    Add Projects Model

---
+++ LaravelRegio/app/Models/Projects.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Projects extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'project_name',
+        'required_job_position',
+        'talent_requirement',
+        'project_description',
+        'project_location',
+        'project_start_date',
+        'project_end_date',
+        'deadline',
+        'salary_min',
+        'salary_max',
+        'status',
+        'talent_needed_min',
+        'talent_needed_max',
+    ];
+    
+    public function company(): BelongsTo {
+        return $this->belongsTo(Company::class);
+    }
+}
--- LaravelRegio/app/Models/WorkExperience.php
+++ LaravelRegio/app/Models/WorkExperience.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Work_Experience extends Model
+class WorkExperience extends Model
 {
     use HasFactory;
 
