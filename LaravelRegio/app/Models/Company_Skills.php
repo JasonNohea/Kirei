9b8424b9158249ec57dc1a0cb6d6818285b3f944
Pistonfry <christianfiestyawan@gmail.com>
2023-07-03 14:24:13 +0700

    Add Skills Models

---
+++ LaravelRegio/app/Models/Company_Skills.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Company_Skills extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'company_id',
+        'skill_id',
+    ];
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(Talent::class);
+    }
+}
