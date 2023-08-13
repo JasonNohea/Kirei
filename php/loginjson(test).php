9674d1c297673dfcc5b64a8d9a1d374d5d5b36e0
HendiC <juhen212212@gmail.com>
2023-08-14 01:10:21 +0700

    UPDATE coz there are error anywhere (html_talent only)

---
+++ php/loginjson(test).php
@@ -0,0 +1,30 @@
+<?php
+$database = file_get_contents("../json/account.json");
+$database = json_decode($database, TRUE);
+if (isset($_POST['email']) && isset($_POST['password'])) {
+    for ($i = 0; $i < count($database); $i++) {
+        if ($database[$i]['email'] == $_POST['email']) {
+            if ($database[$i]['password'] == $_POST['password']) {
+                $success = TRUE;
+                break;
+            } else {
+                $success = FALSE;
+            }
+        } else {
+            $success = FALSE;
+        }
+    }
+} else {
+    echo "Harap isi semua kolom yang tersedia";
+}
+if ($success == TRUE) {
+    echo "Selamat Datang " . $_POST['email'];
+    //membuat metode redirect dengan kode 301
+    header("location: ../html_talent/hometalent-loggedin.html", true, 301);
+    //membuat kode di bawah header tidak diproses oleh website sehingga lebih aman
+    exit();
+} else {
+    echo "Username/Password Salah";
+    header("location: ../html_talent/hometalent.html", true, 301);
+    exit();
+}
